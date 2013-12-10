4a05c1c993e0e624c5248851734a99b7e34bf36b
MarK <mark@mark-K50IJ.(none)>
2013-12-10 22:17:06 +0100

    FkSolution fonctionnelle.

--- src/part2/AbstractSolution.java
+++ src/part2/AbstractSolution.java
@@ -5,15 +5,17 @@ import java.awt.event.WindowAdapter; // to close
 import java.awt.event.WindowEvent; // to close
 
 public abstract class AbstractSolution extends Frame {
-    protected static final int WIDTH = 800; // initial frame dimensions
-    protected static final int HEIGHT = 1200;
+    protected static final int WIDTH = 600; // initial frame dimensions
+    protected static final int HEIGHT = 600;
     protected int depth; // recursion depth
+    protected int initialLength;
 
-    public AbstractSolution(int depth) {
+    public AbstractSolution(int depth, int length) {
         this.setSize(WIDTH, HEIGHT);
         this.setVisible(true);
         this.setBackground(Color.white);
         this.depth = depth;
+        initialLength = length;
         addWindowListener(new WindowHandler()); // to close
         setTitle("Dessins recursifs au niveau : " + depth);
     }
@@ -32,20 +34,19 @@ public abstract class AbstractSolution extends Frame {
         int frameHeight = getSize().height; // frame dimensions with borders
         int frameWidth = getSize().width;
         g2d.setColor(Color.black); // the color we are going to draw with
+
         // g2d.translate(frameWidth/2,frameHeight/2); //to move the origin if
         // necessary
 
-        // call here drawSolutionk with an adequate list of arguments, for
-        // instance :
-        // drawSolutionk(g2d, frameWidth/20, frameHeight/20,
-        // frameWidth/4, depth);
+        // drawSolutionk call
+        drawSolutionk(g2d, 1, getInsets().top + 1, initialLength,
+                depth);
     }
 
     /**
-     * drawSolutionk: Produce a recursive drawing Parameters: Graphics
+     * drawSolutionk: Produces a recursive drawing Parameters: Graphics
      * drawingArea: The graphic object in which we draw int ... arg : the
      * coordinates, length and others if necessary, and recursion depth
-     **/
-    // public abstract void drawSolutionk(Graphics drawingArea, int ... arg) ;
-
+     */
+    public abstract void drawSolutionk(Graphics drawingArea, int... arg);
 }
--- src/part2/FkSolution.java
+++ src/part2/FkSolution.java
@@ -1,10 +1,22 @@
 package part2;
 
+import java.awt.Graphics;
+
 public class FkSolution extends AbstractSolution {
 
-    public FkSolution(int depth) {
-        super(depth);
-        // TODO Auto-generated constructor stub
+    public FkSolution(int depth, int diameter) {
+        super(depth, diameter);
+    }
+
+    @Override
+    public void drawSolutionk(Graphics drawingArea, int... args) {
+        drawingArea.drawOval(args[0], args[1], args[2], args[2]);
+        if (args[3] != 0) {
+            drawSolutionk(drawingArea, args[0] + args[2], args[1] + args[2]/4,
+                    args[2] / 2, args[3] - 1);
+            drawSolutionk(drawingArea, args[0] + args[2]/4, args[1] + args[2],
+                    args[2] / 2, args[3] - 1);
+        }
     }
 
     /**
@@ -12,7 +24,7 @@ public class FkSolution extends AbstractSolution {
      * @param args
      */
     public static void main(String[] args) {
-        FkSolution fks = new FkSolution(0);
+        FkSolution fks = new FkSolution(5, 200);
     }
 
 }
